922316e5bfcd6fb04b7d0a17e201d96842c26e1c
cssoftware <0@1.null>
2021-10-09 19:37:46 -0700

    continuing implementation of sprite renderer

--- D3D12Prototyping/SpriteRenderer.cpp
+++ D3D12Prototyping/SpriteRenderer.cpp
@@ -1,11 +1,27 @@
 #include "pch.h"
 #include "SpriteRenderer.h"
 
+SpriteRenderer::SpriteRenderer(ID3D12Device* pGPU)
+{
+}
+
+SpriteRenderer::~SpriteRenderer()
+{
+}
+
 void SpriteRenderer::Initialize(uint32 backBufferCount)
 {
 	numBackbuffers = backBufferCount;
 
 	InitializeGPUMemory();
+}
+
+void SpriteRenderer::BeginRenderPass(uint32 frameIndex)
+{
+}
+
+void SpriteRenderer::EndRenderPass()
+{
 }
 
 void SpriteRenderer::InitializeGPUMemory()
@@ -16,4 +32,15 @@ void SpriteRenderer::InitializeGPUMemory()
 	{
 		PerFrameMemory = new LinearConstantBuffer(GPU, 16); //16MB of per-frame dynamic data
 	}
+}
+
+void SpriteRenderer::InitializePipelineState()
+{
+	CD3DX12_ROOT_PARAMETER1 rootParameters[3];
+
+	rootParameters[0].InitAsConstantBufferView(0, 0, D3D12_ROOT_DESCRIPTOR_FLAG_NONE, D3D12_SHADER_VISIBILITY_VERTEX);
+	rootParameters[1].InitAsConstantBufferView(1, 0, D3D12_ROOT_DESCRIPTOR_FLAG_NONE, D3D12_SHADER_VISIBILITY_VERTEX);
+	
+	CD3DX12_DESCRIPTOR_RANGE1 ranges[1];
+	ranges[0].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0, 0, D3D12_DESCRIPTOR_RANGE_FLAG_DATA_STATIC);
 }
