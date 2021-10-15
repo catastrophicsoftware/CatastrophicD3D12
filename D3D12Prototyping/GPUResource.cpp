decf9a5ad63235e5aeea7de1c23ae31ca285d565
cssoftware <0@1.null>
2021-10-14 19:42:43 -0700

    rough initial implementation of IGPUResource class

--- D3D12Prototyping/GPUResource.cpp
+++ D3D12Prototyping/GPUResource.cpp
@@ -1,15 +1,80 @@
 #include "pch.h"
 #include "GPUResource.h"
+#include "DeviceResources.h"
 
-GPUResource::GPUResource(ID3D12Device* pGPU)
+using namespace DirectX;
+
+IGPUResource::IGPUResource(uint64 id, DX::DeviceResources* pEngine, ID3D12Resource* pResourceHandle, bool cpuAccessible, D3D12_RESOURCE_STATES initialState, CENGINE_GPU_RESOURCE_TYPE type)
+{
+    assert(pEngine != nullptr && pResourceHandle != nullptr);
+    Engine = pEngine;
+    GPU = Engine->GetD3DDevice();
+    Handle = pResourceHandle;
+    this->cpuAccessible = cpuAccessible;
+    state = initialState;
+    desc = pResourceHandle->GetDesc();
+    gpuAddress = Handle->GetGPUVirtualAddress();
+    mapped = false;
+    resourceType = type;
+}
+
+IGPUResource::IGPUResource() : GPU(nullptr),
+    Handle(nullptr),
+    id(0),
+    cpuAccessible(false),
+    mapped(false),
+    state(D3D12_RESOURCE_STATE_COMMON),
+    gpuAddress(0),
+    resourceType(Undefined),
+    pMappedMemory(nullptr),
+    desc({})
 {
 }
 
-GPUResource::~GPUResource()
+IGPUResource::~IGPUResource()
 {
 }
 
-uint64 GPUResource::Id() const
+uint64* IGPUResource::Map()
+{
+    assert(cpuAccessible);
+
+    if (mapped)
+        return pMappedMemory;
+    else
+    {
+        CD3DX12_RANGE readRange{};
+        void* pTempMap;
+        ThrowIfFailed(Handle->Map(0, &readRange, &pTempMap));
+        mapped = true;
+        pMappedMemory = static_cast<uint64*>(pTempMap);
+        return pMappedMemory;
+    }
+}
+
+void IGPUResource::Unmap()
+{
+    assert(cpuAccessible && mapped);
+    Handle->Unmap(0, nullptr);
+    mapped = false;
+}
+
+uint64 IGPUResource::Id() const
 {
     return id;
+}
+
+D3D12_RESOURCE_STATES IGPUResource::GetState() const
+{
+    return D3D12_RESOURCE_STATES();
+}
+
+D3D12_RESOURCE_DESC IGPUResource::GetDescription() const
+{
+    return desc;
+}
+
+CENGINE_GPU_RESOURCE_TYPE IGPUResource::GetType() const
+{
+    return resourceType;
 }
--- D3D12Prototyping/GPUResource.h
+++ D3D12Prototyping/GPUResource.h
@@ -1,27 +1,59 @@
 #pragma once
 #include "pch.h"
+#include "DirectXHelpers.h"
+#include "PlatformHelpers.h"
 
 enum CENGINE_GPU_RESOURCE_TYPE
 {
-	BUFFER = 1,
-	TEXTURE = 2,
+	Undefined = 0,
+	Buffer = 1,
+	Texture = 2,
 };
 
 struct InflightResourceRegion
 {
 	uint64 fenceValue;
+	union ResourceRange
+	{
+		CD3DX12_RANGE bufferRange;
+		CD3DX12_BOX textureBox;
+	};
 };
 
-class GPUResource
+namespace DX
+{
+	class DeviceResources;
+}
+
+class IGPUResource
 {
 public:
-	GPUResource(ID3D12Device* pGPU);
-	~GPUResource();
+	IGPUResource();
+	IGPUResource(uint64 id,DX::DeviceResources* Engine, ID3D12Resource* pResourceHandle, bool cpuAccessible, D3D12_RESOURCE_STATES initialState, CENGINE_GPU_RESOURCE_TYPE type);
+	virtual ~IGPUResource() = 0;
+
+	uint64* Map();
+	void Unmap();
+
+	virtual void Destroy() = 0;
 
 	uint64 Id() const;
+	D3D12_RESOURCE_STATES GetState() const;
+	D3D12_RESOURCE_DESC GetDescription() const;
+	CENGINE_GPU_RESOURCE_TYPE GetType() const;
 protected:
+	ID3D12Device* GPU;
+	ID3D12Resource* Handle;
+	DX::DeviceResources* Engine;
+
 	uint64 id;
 
 	uint32 cpuAccessible : 1;
 	uint32 mapped : 1;
+	uint64* pMappedMemory;
+
+	D3D12_RESOURCE_DESC desc;
+	D3D12_RESOURCE_STATES state;
+	D3D12_GPU_VIRTUAL_ADDRESS gpuAddress;
+	CENGINE_GPU_RESOURCE_TYPE resourceType;
 };
