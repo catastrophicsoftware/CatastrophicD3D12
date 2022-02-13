d817882d4a57975695c4a18a69de5fd09bae38a6
cssoftware <0@1.null>
2022-02-13 14:39:12 -0800

    organized better

--- D3D12Prototyping/simple_ps.hlsl
+++ D3D12Prototyping/simple_ps.hlsl
@@ -1,20 +1,24 @@
 cbuffer CBMaterial : register(b0)
 {
-	float4 color;
+	float4 materialDiffuseColor;
 }
 
+struct PixelInput
+{
+	float4 position : SV_Position;
+	float4 normal : NORMAL;
+	float2 uv : TEXCOORD;
+};
 
-float4 main(float4 position : SV_Position, float4 normal : NORMAL, float2 uv : TEXCOORD) : SV_TARGET
+float4 main(PixelInput input) : SV_TARGET
 {
 	float4 output = float4(0,0,0,0);
 
 	float4 lightColor = float4(1, 1, 1, 1);
 	float3 lightDirection = float3(0, 1, 0);
 
-	float4 diffuselight = float4(1, 1, 1, 1) * dot(normal, lightDirection);
-
-
-	output += float4(1, 1, 1, 1) * 0.5f; //ambient light
+	float4 diffuselight = float4(1, 1, 1, 1) * dot(input.normal, lightDirection);
+	output += float4(1, 1, 1, 1) * 0.1f; //ambient light
 
 	output += diffuselight;
 	output.a = 1.0f;
